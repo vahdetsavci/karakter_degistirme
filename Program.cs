4b2b68de25498b13fc074e583047b671c032b2fc
Vahdet Savci <savcivahdet@gmail.com>
2024-01-11 16:21:21 +0300

    app is done

--- Program.cs
+++ Program.cs
@@ -6,6 +6,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Baslangic:
+        Console.Write("\n Bir değer giriniz! \n ------------------ \n");
+        char[] chars = Console.ReadLine().ToCharArray();
+        Array.Reverse(chars);
+        Console.WriteLine(chars);
+
+        Secim:
+        Console.Write("\n Yeni değer girmek ister misiniz? \n E: Evet \n H: Hayır \n");
+        switch (Console.ReadLine().ToLower())
+        {
+            case "e":
+                goto Baslangic;
+            case "h":
+                break;
+            default:
+                Console.WriteLine("Geçersiz seçim yaptınız, lütfen size sunulan seçeneklerin dışına çıkmayın!");
+                goto Secim;
+        }
     }
 }
